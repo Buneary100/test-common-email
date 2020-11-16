bb6d80dce3a1f312630cf1fef0e05efd08db74f7
Lynsey Nguyen <lynseyn@umich.edu>
2020-11-15 20:02:27 -0500

    Added tests for addBcc

---
+++ commons-email-1.3.2-src/src/test/java/EmailConcrete.java
@@ -0,0 +1,12 @@
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+
+
+public class EmailConcrete extends Email {
+
+	@Override
+	public Email setMsg(String msg) throws EmailException{
+		
+		return null;
+	}
+}
---
+++ commons-email-1.3.2-src/src/test/java/EmailTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.mail.EmailException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmailTest {
+	private static String[] TEST_EMAILS = {"ab@bc.com", "a.b@c.org", "abcdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd"};
+	private static String[] BAD_ARRAY = {};
+
+	private EmailConcrete email;
+	
+	@Before
+	public void setUpEmailTest() throws Exception{
+		email = new EmailConcrete();
+	}
+	
+	@After
+	public void tearDownEmailTest() throws Exception{}
+	
+	//Tests begin here
+	
+		@Test (expected = EmailException.class )
+		public void testAddBccNull() throws Exception{
+			email.addBcc(BAD_ARRAY);
+		}
+		
+		@Test
+		public void testAddBcc() throws Exception{
+			email.addBcc(TEST_EMAILS);
+			assertEquals(3, email.getBccAddresses().size());
+		}
+}
